2088e9c635af2958e3325fd5ac112de8373c4272
misammehmannavaz <misammehmannavaz@gmail.com>
2023-07-11 18:50:05 +0330

    Added 2 models

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class menu(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    invertory = models.SmallIntegerField()
+    
+    def __str__(self):
+        return self.title + " - " + str(self.price) + " - " + self.invertory
+
+class booking(models.Model):
+    name = models.CharField(max_length=200)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.name + " - " + str(self.no_of_guests) + " - " + str(self.booking_date)
