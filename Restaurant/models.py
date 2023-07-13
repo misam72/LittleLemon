dbe5ee5fbe3a02e2a5984bd6e3c9e16781419ebc
misammehmannavaz <misammehmannavaz@gmail.com>
2023-07-13 11:56:32 +0330

    update models

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 
+
 class menu(models.Model):
     title = models.CharField(max_length=200)
     price = models.FloatField()
     inventory = models.SmallIntegerField()
-    
+
     def __str__(self):
-        return self.title + " - " + str(self.price) + " - " + str(self.inventory)
+        return self.title
+
+    def get_item(self):
+        return f"{self.title} : {self.price}"
+
 
 class booking(models.Model):
     name = models.CharField(max_length=200)
     no_of_guests = models.IntegerField()
     booking_date = models.DateTimeField()
-    
+
     def __str__(self):
-        return self.name + " - " + str(self.no_of_guests) + " - " + str(self.booking_date)
+        return self.name
