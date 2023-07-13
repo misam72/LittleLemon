6361f11022db6ada7bfd36c27e06c1311fbd1ae2
misammehmannavaz <misammehmannavaz@gmail.com>
2023-07-13 12:00:51 +0330

    added a new url

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('users/', views.UserView.as_view()),
     path('users/<int:pk>', views.SingleUserView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
