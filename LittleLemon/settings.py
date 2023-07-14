509edeb6fafb349cf1b37ba0b1d574f720c9e086
misammehmannavaz <misammehmannavaz@gmail.com>
2023-07-14 11:49:33 +0330

    Added authentication section.

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'Restaurant',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+RESTFRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES" : (
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    )
+}
+
+DJOSER = {"USER_ID_FIELD":"username"}
